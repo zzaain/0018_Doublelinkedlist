108aab1a1c61623326d102bf14e6b3529b60ec89
zzaain <sheriellaghzain@gmail.com>
2026-04-30 19:47:41 +0700

    menambahkan switchcase menu

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -219,6 +219,33 @@ int main()
     do
     {
         cout << "\nMenu:\n";
+        cout << "1. Add Record\n";
+        cout << "2. Delete Record\n";
+        cout << "3. View Ascending\n";
+        cout << "4. View Descending\n";
+        cout << "5. Search Record\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.revtraverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
        
 
     } while (choice != '6');
