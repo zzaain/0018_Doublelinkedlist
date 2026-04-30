f02f28d150c38adb7ae8c19117853d91f3a7f3c3
zzaain <sheriellaghzain@gmail.com>
2026-04-30 19:43:27 +0700

    menambahkan fungsi raverse traverse

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -151,4 +151,33 @@ void hapus()
         }
     }
 
-    
+    void revtraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        // Step 1: Move to last node
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        // Step 2: Traverse backward
+        cout << "\nRecords in descending order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            // Step 3: Move to previous node
+            currentNode = currentNode->prev;
+            i--;
+        }
+    }
+
+   
