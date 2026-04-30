84155fb506ac7eed8a87c7079194fc74d34b6e72
zzaain <sheriellaghzain@gmail.com>
2026-04-30 19:42:14 +0700

    menambahkan fungsi traverse

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -127,4 +127,28 @@ void hapus()
         cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
 
+    void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        // Step 1: Mark first node as currentNode
+        Node *currentNode = START;
+
+        // Step 2: Repeat until currentNode == NULL
+        cout << "\nRecords in ascending order of roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            // Step 3: Move to next node
+            currentNode = currentNode->next;
+            i++;
+        }
+    }
+
     
